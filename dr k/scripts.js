38273f88b8a88a7444b8e7652ea6be3d2911e4e0
Luke Thayer <148821490+and-then-theres-luke@users.noreply.github.com>
2024-09-03 07:27:29 -0700

    more stuff I guess

--- dr k/scripts.js
+++ dr k/scripts.js
@@ -1,94 +1,76 @@
 // alert("Linked!")
 
-// popup controls
+// POPUP FUNCTION
 
-document.addEventListener("DOMContentLoaded", function()
-    {
-        function showPopUp() {
-            const popUpScreen = document.getElementById("popUpScreen")
-            popUpScreen.classList.remove("hidden-pop-up")
-        }
-        setTimeout(showPopUp, 10000)
+document.addEventListener("DOMContentLoaded", function () {
+    function showPopUp() {
+        const popUpScreen = document.getElementById("popUpScreen");
+        popUpScreen.classList.remove("hidden-pop-up");
     }
-
-)
+    setTimeout(showPopUp, 10000);
+});
 
 function removePopUp() {
-    const popUpScreen = document.getElementById("popUpScreen")
-    popUpScreen.style.display = "none"
+    const popUpScreen = document.getElementById("popUpScreen");
+    popUpScreen.style.display = "none";
 }
 
-//
+//SCROLL FUNCTIONS
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            entry.target.classList.add("show")
-        }
-        else {
-            entry.target.classList.remove("show")
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
         }
     });
-})
+});
 
-const hiddenElements = document.querySelectorAll(".hidden")
-hiddenElements.forEach((el) => observer.observe(el))
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
 
 window.addEventListener("scroll", function () {
     const navbar = document.getElementById("navbar");
-    const links = document.querySelectorAll(".navlink")
+    const links = document.querySelectorAll(".navlink");
     if (window.scrollY > 50) {
         navbar.classList.add("scrolled");
-        links.forEach((link) => link.classList.add("darklink"))
+        links.forEach((link) => link.classList.add("darklink"));
     } else {
         navbar.classList.remove("scrolled");
-        links.forEach((link) => link.classList.remove("darklink"))
+        links.forEach((link) => link.classList.remove("darklink"));
     }
 });
 
-
+// SLIDESHOW FUNCTIONS
 let slideIndex = 1;
-showSlides(slideIndex)
+showSlides(slideIndex);
 
 function plusSlides(n) {
-    slides = document.getElementsByClassName("mySlides")
-    slides[slideIndex - 1].style.display = "none"
-    showSlides(slideIndex += n)
+    slides = document.getElementsByClassName("mySlides");
+    slides[slideIndex - 1].style.display = "none";
+    showSlides((slideIndex += n));
 }
 
 function showSlides(n) {
-    slides = document.getElementsByClassName("mySlides")
+    slides = document.getElementsByClassName("mySlides");
     if (n > slides.length) {
         slideIndex = 1;
     }
-    if (n < 1) { slideIndex = slides.length}
-    slides[slideIndex - 1].style.display  = "block";
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    slides[slideIndex - 1].style.display = "block";
 }
 
-// function plusSlides(n) {
-//     slides = document.getElementsByClassName("mySlides")
-//     slides[slideIndex - 1].target.classList.remove("slideShow")
-// }
-
-// function showSlides(n) {
-//     slides = document.getElementsByClassName("mySlides")
-//     if (n > slides.length) {
-//         slideIndex = 1;
-//     }
-//     if (n < 1) {
-//         slideIndex = slides.length - 1
-//     }
-//     slides[slideIndex - 1].target.classList.add("slideShow")
-// }
-
+// DARKMODE FUNCTIONS
 function darkmode() {
-    let main = document.getElementById("body")
+    let main = document.getElementById("body");
     if (main.classList.contains("darkmode")) {
-        main.classList.remove("darkmode")
-        main.classList.add("lightmode")
+        main.classList.remove("darkmode");
+        main.classList.add("lightmode");
     } else {
-        main.classList.remove("lightmode")
-        main.classList.add("darkmode")
+        main.classList.remove("lightmode");
+        main.classList.add("darkmode");
     }
-
 }
